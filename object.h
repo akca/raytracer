8a47e89b6479c4ff02fe2a3d5b3f363f0e25731b
Fatih Akca <fatihakca5@gmail.com>
2017-10-26 01:10:11 +0300

    ses yok goruntu var

---
+++ object.h
@@ -0,0 +1,38 @@
+#ifndef OBJECT_H__
+#define OBJECT_H__
+
+#include "parser.h"
+
+class Object 
+{
+	public: 
+		Object() : color(255, 255, 255) {} 
+		virtual ~Object() {} 
+
+		virtual bool intersects(const Vector3D &, const Vector3D &, float &) const = 0; 
+		virtual void getSurfaceData(const Vector3D &, Vector3D &) const = 0; 
+		
+		Vector3D color; // TODO change data type!
+};
+
+bool quadraticSolve(const float &a, const float &b, const float &c, float &t0, float &t1)
+{ 
+	float delta = b * b - (4 * a * c);
+	//std::cout << delta << std::endl;
+	
+	if (delta < 0) return false; //no soln
+	else if (delta == 0) { // TODO impossible !!! FIXME
+		t0 = t1 = - 0.5 * b / a;
+	}
+	else {
+		float q = (b > 0) ? 
+			(b + sqrt(delta)) * -0.5 : 
+			(b - sqrt(delta)) * -0.5; 
+		t0 = q / a; 
+		t1 = c / q;
+    }
+ 
+    return true; 
+} 
+
+#endif
---
+++ sphere.h
@@ -0,0 +1,53 @@
+#ifndef SPHERE_H__
+#define SPHERE_H__
+
+#include "vector3d.h"
+#include "ray.h"
+#include "object.h"
+
+class Sphere : public Object {
+	public:
+		Vector3D center; // center of the sphere
+		float r;
+		float r2;
+
+		Sphere (Vector3D& c, float radius) {
+			center = c;
+			r = radius;
+			r2 = r * r;
+		}
+
+		bool intersects(const Vector3D &origin, const Vector3D &direction, float &t) const {
+
+			Vector3D L = origin - center;
+
+			float b = 2 * direction.dotProduct(L);
+			float c = L.dotProduct(L) - r2;
+			//std::cout << "ORG: " << origin.x << " " << origin.y << " " << origin.z << std::endl;
+			std::cout << "EHEH: " << 1 << " " << b << " " << c << std::endl;
+			float t0, t1; // these will become roots
+			if (!quadraticSolve(1, b, c, t0, t1)) return false;
+
+			if (t0 > t1) std::swap(t0, t1);
+			std::cout << "ilginc: " << t0 << " " << t1 << std::endl;
+
+			if (t1 < 0) return false; // both roots are negative, no intersection
+
+			if (t0 < 0) {
+				t = t1; // if t0 is negative, t1 should be the visible param.
+			} else {
+				t = t0;
+			}
+
+			return true;
+		}
+
+		void getSurfaceData(const Vector3D &Phit, Vector3D &Nhit) const
+		{
+			Nhit = Phit - center;
+			Nhit.normalize();
+		}
+};
+
+#endif
+
