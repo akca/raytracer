50fae3483cd1320e7554a7860650b27263314deb
Fatih Akca <fatihakca5@gmail.com>
2017-11-28 23:59:55 +0300

    defining functions as INLINE makes everything x2 faster!

--- triangle.h
+++ triangle.h
@@ -25,9 +25,53 @@ public:
     texture_id = texture;
   }
 
-  bool intersects(const Vector3D &origin, const Vector3D &direction, float &t,
-                  Vector3D &intersectPoint, Vector3D &normal, bool isShadowRay,
-                  Vec2f &texCoordData);
+  bool intersects(const Vector3D &origin, const Vector3D &direction,
+                  float &tmin, Vector3D &intersectPoint, Vector3D &norml,
+                  bool isShadowRay, Vec2f &texCoord) {
+
+    Vector3D pVector = direction * edge2;
+    float det = edge1.dotProduct(pVector);
+
+    // do back-face culling if not shadow ray.
+    // otherwise ignore rays that orthogonal to triangle normal.
+    if ((!isShadowRay && det < 0.0f) || (isShadowRay && fabs(det) < 0.0f))
+      return false;
+
+    float inverseDet = 1 / det;
+    float u, v;
+
+    Vector3D tVector = origin - v1;
+    u = inverseDet * tVector.dotProduct(pVector);
+    if (u < -1e-7 || u > 1.00001) {
+      return false;
+    }
+
+    Vector3D qVector = tVector * edge1;
+    v = inverseDet * direction.dotProduct(qVector);
+    if (v < -1e-7 || u + v > 1.00001) {
+      return false;
+    }
+
+    float tmin_new = inverseDet * edge2.dotProduct(qVector);
+
+    if (tmin_new > 0.0f && tmin_new < tmin) {
+      tmin = tmin_new;
+
+      if (!isShadowRay) {
+        norml = normal;
+        if (texture_id != -1) {
+          texCoord = {texCoord1.x + u * (texCoord2.x - texCoord1.x) +
+                          v * (texCoord3.x - texCoord1.x),
+                      texCoord1.y + u * (texCoord2.y - texCoord1.y) +
+                          v * (texCoord3.y - texCoord1.y)};
+        }
+      }
+
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 #endif
--- vector3d.h
+++ vector3d.h
@@ -9,27 +9,94 @@ public:
   float y;
   float z;
 
-  Vector3D();
-  Vector3D(const Vector3D &v);
-  Vector3D(float i, float j, float k);
-  float getLength() const;
-  Vector3D &normalize();
-  Vector3D inverse() const;
-  Vector3D operator-(const Vector3D &v) const;
-  Vector3D operator+(const Vector3D &v) const;
-  float dotProduct(const Vector3D &v) const;
-  Vector3D multiply(const Vector3D &v) const;
-  void applyTransform(const float *v, bool isVector);
-  float distance(const Vector3D &v) const;
+  float getLength() const { return sqrtf(x * x + y * y + z * z); }
 
+  Vector3D() {
+    x = 0;
+    y = 0;
+    z = 0;
+  }
+
+  Vector3D(const Vector3D &v) {
+    x = v.x;
+    y = v.y;
+    z = v.z;
+  }
+
+  Vector3D(float i, float j, float k) {
+    x = i;
+    y = j;
+    z = k;
+  }
+
+  Vector3D &normalize() {
+
+    float length = getLength();
+
+    x /= length;
+    y /= length;
+    z /= length;
+
+    return *this;
+  }
+
+  Vector3D inverse() const { return Vector3D(-x, -y, -z); }
+
+  Vector3D operator-(const Vector3D &v) const {
+    return Vector3D(x - v.x, y - v.y, z - v.z);
+  }
+
+  Vector3D operator+(const Vector3D &v) const {
+    return Vector3D(x + v.x, y + v.y, z + v.z);
+  }
   // Divide by constant
-  Vector3D operator/(float cons) const;
+  Vector3D operator/(float cons) const {
+    return Vector3D(x / cons, y / cons, z / cons);
+  }
 
   // Multiply by constant
-  Vector3D operator*(float cons) const;
+  Vector3D operator*(float cons) const {
+    return Vector3D(x * cons, y * cons, z * cons);
+  }
 
   // Vector cross product
-  Vector3D operator*(const Vector3D &v) const;
+  Vector3D operator*(const Vector3D &v) const {
+    return Vector3D(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+  }
+
+  float dotProduct(const Vector3D &v) const {
+    return x * v.x + y * v.y + z * v.z;
+  }
+
+  Vector3D multiply(const Vector3D &v) const {
+    return Vector3D(x * v.x, y * v.y, z * v.z);
+  }
+
+  float distance(const Vector3D &v) const {
+    return sqrtf(pow((x - v.x), 2) + pow((y - v.y), 2) + pow((z - v.z), 2));
+  }
+
+  void applyTransform(const float *v, bool isVector) {
+
+    float newX = v[0] * x + v[1] * y + v[2] * z;
+    float newY = v[4] * x + v[5] * y + v[6] * z;
+    float newZ = v[8] * x + v[9] * y + v[10] * z;
+
+    if (!isVector) {
+      newX += v[3];
+      newY += v[7];
+      newZ += v[11];
+    }
+    /*
+      std::cout << x << " " << y << " " << z << std::endl;
+      std::cout << newX << " " << newY << " " << newZ << std::endl;
+      std::cout << std::endl;
+    */
+    x = newX;
+    y = newY;
+    z = newZ;
+  }
+
 };
 
 #endif
